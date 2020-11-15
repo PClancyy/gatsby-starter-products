ed4a8743d8796990869c90af102129058a391e76
PClancyy <36280985+PClancyy@users.noreply.github.com>
2020-11-15 20:02:44 +0000

    fixed config icon and about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 
+
 class About extends React.Component {
     render() {
         return ( <Layout>
@@ -11,9 +12,10 @@ class About extends React.Component {
                     <div className="container">
                     <div className="row">
                         <div className="col-sm-12 col-md-4 align-self-center">
-                        <h1>Established November 2020</h1>
-                        <p>Featuring all of your favourite PACHAWHITEAESTHETIC.GLA products</p>
-                         <p>New stock updated regularly and limited time deals ❤</p>
+                        <h6>Established November 2020.</h6>
+                        <p>Featuring all of your favourite PACHAWHITEAESTHETIC.GLA products.</p>
+                         <p>New stock updated regularly.</p>
+                         <p>Limited time deals ❤</p>
                         </div>
                         </div>
                     </div>
